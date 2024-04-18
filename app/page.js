08fac75f050edb2129dc0e8a23c24b6ecbc67aeb
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-04-18 17:42:22 +0100

    Project home page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import Link from 'next/link';
 
-const HomePage = () => {
-    return <div>HomePage</div>;
-};
-
-export default HomePage;
+export default function Home() {
+    return (
+        <div>
+            <h1 className="text-5xl mb-8 font-bold">Next.js Tutorial</h1>
+            <Link href="/client" className="btn btn-accent">
+                get started
+            </Link>
+        </div>
+    );
+}
