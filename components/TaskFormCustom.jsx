34a495eb81c82fdf50a65663c771dc4407b8ee2d
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-04-18 16:56:46 +0100

    Create TaskFormCustom component

---
+++ components/TaskFormCustom.jsx
@@ -0,0 +1,54 @@
+'use client';
+
+import {useEffect} from 'react';
+import toast from 'react-hot-toast';
+import {useFormStatus, useFormState} from 'react-dom';
+import {createTaskCustom} from '@/utils/actions';
+// The useFormStatus Hook provides status information of the last form submission.
+// useFormState is a Hook that allows you to update state based on the result of a form action.
+
+const SubmitBtn = () => {
+    const {pending} = useFormStatus();
+    return (
+        <button
+            type="submit"
+            className="btn btn-primary join-item"
+            disabled={pending}>
+            {pending ? 'please wait...' : 'create task'}
+        </button>
+    );
+};
+
+const initialState = {
+    message: null,
+};
+
+const TaskForm = () => {
+    const [state, formAction] = useFormState(createTaskCustom, initialState);
+
+    useEffect(() => {
+        if (state.message === 'error') {
+            toast.error('there was an error');
+            return;
+        }
+        if (state.message) {
+            toast.success('task created....');
+        }
+    }, [state]);
+
+    return (
+        <form action={formAction}>
+            <div className="join w-full">
+                <input
+                    type="text "
+                    className="input input-bordered join-item w-full"
+                    placeholder="type here"
+                    name="content"
+                    required
+                />
+                <SubmitBtn />
+            </div>
+        </form>
+    );
+};
+export default TaskForm;
